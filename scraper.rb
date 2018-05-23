611755e4696ae3b38e0311f47e6f5eada8627380
Tony Bowden <tony@tmtm.com>
2018-05-23 19:24:42 +0100

    Always fetch pre-existing matches

--- scraper.rb
+++ scraper.rb
@@ -1,7 +1,10 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require 'everypolitician'
 require 'wikidata/fetcher'
 
+existing = EveryPolitician::Index.new.country("Colombia").lower_house.popolo.persons.map(&:wikidata).compact
+
 names = WikiData::Category.new( 'Categoría:Representantes de la Cámara de Colombia 2014-2018', 'es').member_titles
-EveryPolitician::Wikidata.scrape_wikidata(names: { es: names }, output: false)
+EveryPolitician::Wikidata.scrape_wikidata(ids: existing, names: { es: names })
